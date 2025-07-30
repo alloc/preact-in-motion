c8e52a320675ff451ccaee079c079d0c4c35eaa7
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-07-30 01:28:27 -0400

    fix: edge case where AnimatePresence wasn’t detected by descendant

--- src/internal/context.ts
+++ src/internal/context.ts
@@ -3,10 +3,14 @@ import { currentComponent } from './currentComponent'
 
 export function getContextValue<T>(provider: Context<T>): T | undefined {
   if (currentComponent) {
-    const contextId = (provider as any).__c
-    const context = currentComponent.context[contextId]
-    if (context) {
-      return context.props.value
+    const key = (provider as any).__c
+    // Check context of the current component.
+    if (currentComponent.context[key]) {
+      return currentComponent.context[key].props.value
+    }
+    // Check if the current component is the provider we're looking for.
+    if (currentComponent.constructor === provider.Provider) {
+      return currentComponent.props.value
     }
     // Return the default value
     return (provider as any).__
